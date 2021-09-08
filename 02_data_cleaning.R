5551302689829a77b0a12f939eeabc99a7a09c26
michaelagustafson <73973280+michaelagustafson@users.noreply.github.com>
2021-09-08 15:29:25 -0600

    Add files via upload

---
+++ 02_data_cleaning.R
@@ -0,0 +1,24 @@
+### ---------------------------------------------
+
+## Title: Data CLeaning
+
+## Author: Michaela L. Gustafson
+
+## Description: Full data cleaning and merging of 2019 and 2021 prey
+# survey data
+
+## Date created: 08 September 2021
+
+### ---------------------------------------------
+
+### LIBRARY -------------------------------------
+
+library(dplyr)
+
+### END LIBRARY ---------------------------------
+
+### IMPORT DATA ---------------------------------
+
+# Importing the observation and environmental .csv files created
+# in the previous module, 01_import_data.R
+
